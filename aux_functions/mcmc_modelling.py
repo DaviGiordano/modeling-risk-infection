970265816e9bf5c53630e5c55d5304c79762557c
DaviGiordano <davigiordano2704@gmail.com>
2023-12-15 16:09:01 +0000

    refactor mcmc code

--- aux_functions/mcmc_modelling.py
+++ aux_functions/mcmc_modelling.py
@@ -1,27 +1,69 @@
-import pandas as pd
-import arviz as az
 import pymc as pm
+import arviz as az
 
-def get_list_with_specified_dfs(df_complete):
-    """
-    Receives the complete dataset of the study
-    Returns a list with 6 datasets with the variables specified in the report
-    in the order: [complete, top_8, top_5, top_4, top_3, top_2]
-    
-    See variables of each in the report
-    """
-    df_top8 = df_complete.drop(columns=['age', 'region_1', 'region_2', 'region_3', 'region_4',]).astype(float)
-    df_top5 = df_top8.drop(columns=['med_school_affil_1', 'med_school_affil_2', 'avelbl_services', 'ln_num_beds']).astype(float)
-    df_top4 = df_top5.drop(columns='routine_xray_ratio').astype(float)
-    df_top3 = df_top4.drop(columns='ln_avg_census').astype(float)
-    df_top2 = df_top3.drop(columns='lenght_of_stay').astype(float)
-    dfs = [df_complete, df_top8, df_top5, df_top4, df_top3, df_top2]
+def get_specified_dfs(df_complete, variables_of_dfs_to_build):
+    dfs = []
+    for name, variables in variables_of_dfs_to_build.items():
+        dfs.append(df_complete[variables].copy())
     return dfs
 
-def get_list_with_pm_models(dfs):
-    """Returns list of 6 models, for training"""
+def get_models(dfs, models_names, y, y_proposal_distribution="normal"):
     models = []
-    for df in dfs:
-        models.append(pm.Model(coords={'beta_names': df.columns.values}))
-
+    for df, model_name in zip(dfs, models_names):
+        with pm.Model(coords={'beta_names': df.columns.values}, name=model_name) as model:
+            # Priors
+            alpha = pm.Normal("alpha", mu=0, sigma=10)
+            beta = pm.Normal("beta", mu=0, sigma=10, shape=df.shape[1], dims="beta_names") # Number of betas varies with number of covariates in the dataset
+            sigma = pm.HalfNormal("sigma", sigma=10)
+            
+            # Expected value of outcome
+            mu = alpha + pm.math.dot(df, beta)
+            
+            if _is_proposal_distribution_cauchy(y_proposal_distribution):
+                gamma = pm.HalfNormal("gamma", sigma=10) # For Cauchy
+                Y_obs = pm.Cauchy("Y_obs", alpha=mu, beta=gamma, observed=y) # In the docs, the gamma is called beta
+            
+            if _is_proposal_distribution_student(y_proposal_distribution): 
+                nu = pm.HalfNormal("nu", sigma=10) # For TStudent
+                Y_obs = pm.StudentT("Y_obs", mu=mu, sigma=sigma, nu=nu, observed=y)
+            
+            if _is_proposal_distribution_normal(y_proposal_distribution): 
+                Y_obs = pm.Normal("Y_obs", mu=mu, sigma=sigma, observed=y)
+        models.append(model)
     return models
+
+def _is_proposal_distribution_cauchy(y_proposal_distribution):
+    return True if y_proposal_distribution == "cauchy" else False
+
+def _is_proposal_distribution_student(y_proposal_distribution):
+    return True if y_proposal_distribution == "student_t" else False
+
+def _is_proposal_distribution_normal(y_proposal_distribution):
+    return True if y_proposal_distribution == "normal" else False
+
+def sample_models(num_samples, models):
+    sampling_results = []
+    for i, model in enumerate(models):
+        with model:
+            step = pm.Metropolis()
+            sample = _sample_model(num_samples, step)
+            sampling_results.append(sample)
+    return sampling_results
+
+def _sample_model(num_samples, step):
+    return pm.sample(num_samples, step=step, idata_kwargs = {'log_likelihood': True})
+
+# This creates a dictionary with each model name and each sampling result
+def get_identified_sampling_results(sampling_results, models_names):
+    model_trace_dict = dict() # {'model_name': sample_of_model}
+    for i, num in enumerate(models_names):
+        model_trace_dict.update({num: sampling_results[i]})
+    return model_trace_dict
+
+def get_waic_measures(identified_sampling_results):
+    return az.compare(identified_sampling_results, ic='waic')
+
+def get_model_from_list(models, model_name):
+    for model in models:
+        if model.name == model_name:
+            return model
